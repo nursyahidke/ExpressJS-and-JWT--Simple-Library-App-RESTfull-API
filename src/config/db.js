af77145f2e8c4747eb41ee37813ba75ee1dbf2ff
Nursyahid <nursyahidke@gmail.com>
2019-09-01 22:38:04 +0700

    make it better

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,10 @@ require('dotenv').config()
 const mysql = require('mysql')
 
 const conn = mysql.createConnection({
-    host: process.env.DB_HOST, // || 'remotemysql.com' ,
-    user: process.env.DB_USER, // || 'ZOZgkFHa2q',
-    password: process.env.DB_PASSWORD, // || 'm1go6I3Ytq',
-    database: process.env.DB_NAME // || 'ZOZgkFHa2q'
+    host: process.env.DB_HOST || 'remotemysql.com' ,
+    user: process.env.DB_USER || 'ZOZgkFHa2q',
+    password: process.env.DB_PASSWORD || 'm1go6I3Ytq',
+    database: process.env.DB_NAME || 'ZOZgkFHa2q'
 })
 
 module.exports = conn
