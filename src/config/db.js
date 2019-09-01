ea8aa21bd0b72b79991962afa3b8314a5cfcecae
Nursyahid <nursyahidke@gmail.com>
2019-09-01 23:22:45 +0700

    first commit

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,10 @@ require('dotenv').config()
 const mysql = require('mysql')
 
 const conn = mysql.createConnection({
-    host: process.env.DB_HOST, // || 'remotemysql.com' ,
-    user: process.env.DB_USER, // || 'ZOZgkFHa2q',
-    password: process.env.DB_PASSWORD, // || 'm1go6I3Ytq',
-    database: process.env.DB_NAME // || 'ZOZgkFHa2q'
+    host: process.env.DB_HOST || 'localhost' ,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'library'
 })
 
 module.exports = conn
